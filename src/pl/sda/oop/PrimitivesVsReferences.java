a7a30a4f736abbdd02c6cb21ecb141598beb0f93
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2019-02-24 13:57:34 +0100

    primitives vs references example

--- src/pl/sda/oop/PrimitivesVsReferences.java
+++ src/pl/sda/oop/PrimitivesVsReferences.java
@@ -5,7 +5,9 @@ public class PrimitivesVsReferences {
         /*
            Operacje na zmiennych typów pierwotnych
          */
-        int x, y, z;
+        int x;
+        int y;
+        int z;
 
         x = 3;
         y = 4;
@@ -39,8 +41,15 @@ public class PrimitivesVsReferences {
         p1.set(3, 3);
         p2.set(4, 4);
         p3.set(5, 5);
+
         p1 = p2;
+
         p2.set(5, 5);
+        p1.set(9, 9);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
 
         System.out.print("Point p1: ");
         p1.show();
@@ -59,6 +68,24 @@ public class PrimitivesVsReferences {
             System.out.println("p2 == p3");
         } else {
             System.out.println("p2 != p3");
+        }
+
+        Point p4 = new Point();
+        p4.set(6, 6);
+
+        // przypisanie adresu obiektu ze zmiennej p4 do zmiennej p5
+        Point p5 = p4;
+
+        // wyzerowanie adresu zmiennej p4
+        p4 = null;
+
+        System.out.println(p4); // =null
+        System.out.println(p5); // =adres w pamięci
+
+        if (p4 == null) {
+            System.out.println("JEST");
+        } else {
+            System.out.println("NIE JEST");
         }
     }
 }
