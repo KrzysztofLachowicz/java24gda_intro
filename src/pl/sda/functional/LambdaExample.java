1a30704ff7b3775e26bf22c1dc33d1923d27100e
mrzepinski <Maciej.Rzepinski@wolterskluwer.com>
2019-03-17 08:51:16 +0100

    minor fixes

--- src/pl/sda/functional/LambdaExample.java
+++ src/pl/sda/functional/LambdaExample.java
@@ -4,26 +4,27 @@ import pl.sda.generics.Person;
 import pl.sda.generics.Student;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LambdaExample {
-    private static final List<Student> STUDENTS = Arrays.asList(
-        new Student("Maciek", "Rzepiński"),
-        new Student("Jan", "Kowalski"),
-        new Student("Jan", "Nowak")
-    );
-
     public static void main(String[] args) {
-        printLambda();
+        List<Student> students = Arrays.asList(
+                new Student("Maciek", "Rzepiński"),
+                new Student("Jan", "Kowalski"),
+                new Student("Jan", "Nowak")
+        );
 
-        String reduce = STUDENTS.stream()
+        printLambda(students);
+
+        String reduce = students.stream()
             .map(Person::getFirstName)
             .reduce("", (s1, s2) -> s1 + s2);
         System.out.println(reduce);
 
-        List<Student> students = STUDENTS.stream()
+        List<Student> processedList = students.stream()
             .filter(student -> "Jan".equals(student.getFirstName()))
             .map(student -> {
                 System.out.println(student);
@@ -35,18 +36,12 @@ public class LambdaExample {
             .sorted(Comparator.comparing(Person::getFirstName))
             .collect(Collectors.toList());
 
-        if (!students.isEmpty()) {
-            students.forEach(System.out::println);
+        if (!processedList.isEmpty()) {
+            processedList.forEach(System.out::println);
         }
     }
 
-    private static void printForeach() {
-        for (Student student : STUDENTS) {
-            System.out.println(student);
-        }
-    }
-
-    private static void printLambda() {
-        STUDENTS.forEach(System.out::println);
+    private static void printLambda(Collection collection) {
+        collection.forEach(System.out::println);
     }
 }
